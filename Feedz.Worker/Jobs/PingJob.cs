b3d7147fcd50e0a1f0a7ae97f824eb1590b715a2
Mathieu Poussin <git@kedare.net>
2022-05-26 19:26:38 +0200

    Initial commit

---
+++ Feedz.Worker/Jobs/PingJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Hangfire;
+
+namespace Feedz.Worker.Jobs
+{
+	public class PingJob
+	{
+		public static void Run()
+		{
+			BackgroundJob.Enqueue(() => Console.WriteLine("Ping!"));
+		}
+	}
+}
+
---
+++ Feedz.Worker/Program.cs
@@ -0,0 +1,33 @@
+﻿using Hangfire;
+using Hangfire.PostgreSql;
+
+var builder = WebApplication.CreateBuilder(args);
+var hangfireConnectionString = builder.Configuration.GetConnectionString("HangfireConnection");
+
+if(hangfireConnectionString is null) {
+    throw new Exception("Missing HangfireConnection connection string");
+}
+
+// Add services to the container.
+builder.Services.AddHangfire(configuration => configuration
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UsePostgreSqlStorage(hangfireConnectionString)
+    )
+    .AddHangfireServer()
+    .AddAuthorization()
+    .AddControllers();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseHangfireDashboard(pathMatch: "");
+
+app.Run();
+
