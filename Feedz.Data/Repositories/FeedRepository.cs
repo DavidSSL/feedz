ad573303e7c53942fe11e733c59c611efb872ee4
Mathieu Poussin <git@kedare.net>
2022-06-30 22:08:45 +0200

    Add first data repository (needed for mocking)

---
+++ Feedz.Data/Repositories/FeedRepository.cs
@@ -0,0 +1,61 @@
+﻿using Feedz.Data.Database;
+using Feedz.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Feedz.Data.Repositories
+{
+    class FeedRepository : IFeedRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public FeedRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Task<int> CountFeedEntries()
+        {
+            return _db.FeedEntries.CountAsync();
+        }
+
+        public Task<int> CountFeedEntries(Feed feed)
+        {
+            return _db.FeedEntries.Where(fe => fe.Feed == feed).CountAsync();
+        }
+
+        public Task<int> CountFeedEntryUserStates()
+        {
+            return _db.FeedEntryUserStates.CountAsync();
+        }
+
+        public Task<int> CountFeedEntryUserStates(FeedEntry feedEntry)
+        {
+            return _db.FeedEntryUserStates.Where(feus => feus.FeedEntry == feedEntry).CountAsync();
+        }
+
+        public Task<int> CountFeedEntryUserStates(ApplicationUser user)
+        {
+            return _db.FeedEntryUserStates.Where(feus => feus.User == user).CountAsync();
+        }
+
+        public Task<int> CountFeeds()
+        {
+            return _db.Feeds.CountAsync();
+        }
+
+        public Task<int> CountFeedSubscription(Feed feed)
+        {
+            return _db.FeedSubscriptions.Where(fs => fs.Feed == feed).CountAsync();
+        }
+
+        public Task<int> CountFeedSubscriptions()
+        {
+            return _db.FeedSubscriptions.CountAsync();
+        }
+
+        public Task<int> CountFeedSubscriptions(ApplicationUser user)
+        {
+            return _db.FeedSubscriptions.Where(fs => fs.User == user).CountAsync();
+        }
+    }
+}
---
+++ Feedz.Data/Repositories/IFeedRepository.cs
@@ -0,0 +1,17 @@
+﻿using Feedz.Data.Models;
+
+namespace Feedz.Data.Repositories
+{
+    internal interface IFeedRepository
+    {
+        Task<int> CountFeeds();
+        Task<int> CountFeedEntries();
+        Task<int> CountFeedEntries(Feed feed);
+        Task<int> CountFeedSubscriptions();
+        Task<int> CountFeedSubscription(Feed feed);
+        Task<int> CountFeedSubscriptions(ApplicationUser user);
+        Task<int> CountFeedEntryUserStates();
+        Task<int> CountFeedEntryUserStates(FeedEntry feedEntry);
+        Task<int> CountFeedEntryUserStates(ApplicationUser user);
+    }
+}
