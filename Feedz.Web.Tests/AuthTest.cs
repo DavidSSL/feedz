22402881252307247d23fbbc720acdc57adb7247
Mathieu Poussin <git@kedare.net>
2022-06-30 22:09:04 +0200

    Add (for now empty) web test project

---
+++ Feedz.Web.Tests/AuthTest.cs
@@ -0,0 +1,12 @@
+using Feedz.Web.Controllers;
+
+namespace Feedz.Web.Tests
+{
+    public class AuthTest
+    {
+        [Fact]
+        public void AdminIndexPage_DeniedIfAnonymous()
+        {
+        }
+    }
+}
