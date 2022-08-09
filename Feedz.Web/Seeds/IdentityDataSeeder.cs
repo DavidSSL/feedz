a1c85da72df2efea1280ec243604b7abea9dbb77
David S <davidsiew@outlook.com>
2022-08-09 18:41:37 +0200

    Clean up IdentityDataSeeder

--- Feedz.Web/Seeds/IdentityDataSeeder.cs
+++ Feedz.Web/Seeds/IdentityDataSeeder.cs
@@ -2,17 +2,17 @@ using Feedz.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-namespace Feedz.Web.Settings;
+namespace Feedz.Web.Seeds;
 
 public class IdentityDataSeeder
 {
-    private static string adminEmail = "admin@example.com";
-    private static string adminPassword = "secret";
+    private const string AdminEmail = "admin@example.com";
+    private const string AdminPassword = "secret";
 
-    private UserManager<ApplicationUser> _userManager;
-    private RoleManager<ApplicationRole> _roleManager;
+    private readonly UserManager<ApplicationUser> userManager;
+    private readonly RoleManager<ApplicationRole> roleManager;
 
-    private ILogger<IdentityDataSeeder> _logger;
+    private readonly ILogger<IdentityDataSeeder> logger;
 
     public IdentityDataSeeder(
         UserManager<ApplicationUser> userManager,
@@ -20,19 +20,19 @@ public class IdentityDataSeeder
         ILogger<IdentityDataSeeder> logger
     )
     {
-        _logger = logger;
-        _userManager = userManager;
-        _roleManager = roleManager;
-        _logger.LogInformation("Initialized IdentityDataSeeder");
+        this.logger = logger;
+        this.userManager = userManager;
+        this.roleManager = roleManager;
+        this.logger.LogInformation("Initialized IdentityDataSeeder");
     }
 
     public async Task SeedAsync()
     {
-        _logger.LogInformation("Running IdentityDataSeed");
+        logger.LogInformation("Running IdentityDataSeed");
         var needSeed = await IsSeedNeeded();
         if (!needSeed)
         {
-            _logger.LogInformation("No seeding needed");
+            logger.LogInformation("No seeding needed");
             return;
         }
         var administratorRole = new ApplicationRole
@@ -46,55 +46,55 @@ public class IdentityDataSeeder
         var administratorUser = new ApplicationUser()
         {
             Id = Guid.NewGuid(),
-            UserName = adminEmail,
-            NormalizedUserName = adminEmail.ToUpper(),
-            Email = adminEmail,
-            NormalizedEmail = adminEmail.ToUpper(),
+            UserName = AdminEmail,
+            NormalizedUserName = AdminEmail.ToUpper(),
+            Email = AdminEmail,
+            NormalizedEmail = AdminEmail.ToUpper(),
             EmailConfirmed = true
         };
 
-        await CreateUserAsync(administratorUser, adminPassword);
+        await CreateUserAsync(administratorUser, AdminPassword);
 
-        await GrantRoleToUser("Administrator", adminEmail);
+        await GrantRoleToUser("Administrator", AdminEmail);
 
-        _logger.LogInformation("Completed IdentityDataSeeder");
+        logger.LogInformation("Completed IdentityDataSeeder");
     }
 
     private async Task<bool> IsSeedNeeded()
     {
-        var noRoles = await _roleManager.Roles.CountAsync() == 0;
-        var noUsers = await _userManager.Users.CountAsync() == 0;
+        var noRoles = await roleManager.Roles.CountAsync() == 0;
+        var noUsers = await userManager.Users.CountAsync() == 0;
         return noRoles && noUsers;
     }
 
     private async Task CreateRoleAsync(ApplicationRole role)
     {
-        var exits = await _roleManager.RoleExistsAsync(role.Name);
-        if (!exits)
+        var exists = await roleManager.RoleExistsAsync(role.Name);
+        if (!exists)
         {
-            _logger.LogInformation("Creating role {role}", role.NormalizedName);
-            await _roleManager.CreateAsync(role);
+            logger.LogInformation("Creating role {role}", role.NormalizedName);
+            await roleManager.CreateAsync(role);
         }
     }
 
     private async Task CreateUserAsync(ApplicationUser user, string password)
     {
-        var currentUser = await _userManager.FindByEmailAsync(user.Email);
+        var currentUser = await userManager.FindByEmailAsync(user.Email);
         if (currentUser == null)
         {
-            _logger.LogInformation("Creating user {email}", user.Email);
-            await _userManager.CreateAsync(user, password);
+            logger.LogInformation("Creating user {email}", user.Email);
+            await userManager.CreateAsync(user, password);
         }
     }
 
     private async Task GrantRoleToUser(string roleName, string userEmail)
     {
-        var user = await _userManager.FindByEmailAsync(userEmail);
-        var alreadyAssigned = await _userManager.IsInRoleAsync(user, roleName);
+        var user = await userManager.FindByEmailAsync(userEmail);
+        var alreadyAssigned = await userManager.IsInRoleAsync(user, roleName);
         if (!alreadyAssigned)
         {
-            _logger.LogInformation("Granting role {role} to {email}", roleName, userEmail);
-            await _userManager.AddToRoleAsync(user, roleName);
+            logger.LogInformation("Granting role {role} to {email}", roleName, userEmail);
+            await userManager.AddToRoleAsync(user, roleName);
         }
     }
 }
